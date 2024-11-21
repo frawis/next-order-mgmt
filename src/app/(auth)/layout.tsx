cc90ed26376a7a34e91d4c2b0756a8576e4bd829
SupRexT <frank.wissink@btc-echo.de>
2024-11-21 15:25:42 +0100

    chore: :sparkles: add clerk
    integrate clerk into the app

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="w-full max-w-md">
+        {children}
+      </div>
+    </div>
+  )
+}
